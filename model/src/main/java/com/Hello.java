6a83428dba9e793f15c56bfb10c47de95d1a6c02
2592023440@qq.com <2592023440@qq.com>
2016-12-23 02:53:52 +0800

    上传model

---
+++ model/src/main/java/com/Hello.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Hello {
+	@RequestMapping(value = "/say")
+    @ResponseBody
+    public String say(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+    	System.out.println("say"+request.getParameter("hello"));
+        return request.getParameter("hello")+"郅士奇";
+    }
+	
+	@RequestMapping("/index")
+	public ModelAndView index(){
+		Map<String, String> param=new HashMap<String,String>();
+		param.put("name", "郅士奇");
+		return new ModelAndView("index",param);
+	}
+	@RequestMapping("/")
+	public ModelAndView index1(){
+		ModelAndView mav=new ModelAndView("index");
+//		mav.addObject("time", new Date());
+		mav.getModel().put("name", "caoyc");
+		return mav;
+	}	
+}
---
+++ model/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ page isELIgnored ="false" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+<h1>${requestScope.name}</h1>
+</body>
+</html>
